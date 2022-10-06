26a92267c6e01e1bf52ea0483f290c37df21a598
Asim Khalil <108312289+Asimkhalil11@users.noreply.github.com>
2022-10-06 10:38:36 +0500

    Add files via upload

---
+++ 12Greetings.js
@@ -0,0 +1,6 @@
+let myFriends = ["Mujeeb", "Shoaib", "tajammul", "Usama", "Mudassir"]
+console.log("Are you happy? " + myFriends[0])
+console.log("Are you happy? " + myFriends[1])
+console.log("Are you happy? " + myFriends[2])
+console.log("Are you happy? " + myFriends[3])
+console.log("Are you happy? " + myFriends[4])
